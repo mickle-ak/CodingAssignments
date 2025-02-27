f4c768a888e0e53933898249d9b07f5ba9406542
mickle-ak <mickle.ak@gmail.com>
2025-02-27 23:40:54 +0100

    TMS: allow specmatic find breaking changes in included files
    solution: add OpenApi standard header to all files included in OpenAPI definition

--- TaskManagementSystem/SimpleCrudSolution/server/src/test/java/org/mickleak/taskmanagementsystem/server/apiTests/BackwardCompatibilitySpecmaticTest.java
+++ TaskManagementSystem/SimpleCrudSolution/server/src/test/java/org/mickleak/taskmanagementsystem/server/apiTests/BackwardCompatibilitySpecmaticTest.java
@@ -12,7 +12,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class BackwardCompatibilitySpecmaticTest {
 
 	@Test
-	void backwardCompatitbilityTest() throws IOException, InterruptedException {
+	void backwardCompatibilityTest() throws IOException, InterruptedException {
 		String specmaticVersion = "2.5.0";
 		String specmaticJarCanonicalPath = new File( "lib/specmatic-" + specmaticVersion + ".jar" ).getCanonicalPath().replace( '\\', '/' );
 		String gitRoot = executeCommand( "git", "rev-parse", "--show-toplevel" ).output().trim();
