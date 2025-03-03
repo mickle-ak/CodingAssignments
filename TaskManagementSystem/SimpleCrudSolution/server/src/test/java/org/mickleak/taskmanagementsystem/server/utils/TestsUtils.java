119d2990f905c4b7b0fe4142910b433006cede07
kiselev <Mikhail.Kiselev@adesso.de>
2025-03-03 18:15:23 +0100

    TMS: define API for requirements

--- TaskManagementSystem/SimpleCrudSolution/server/src/test/java/org/mickleak/taskmanagementsystem/server/utils/TestsUtils.java
+++ TaskManagementSystem/SimpleCrudSolution/server/src/test/java/org/mickleak/taskmanagementsystem/server/utils/TestsUtils.java
@@ -1,19 +1,20 @@
 package org.mickleak.taskmanagementsystem.server.utils;
 
 import org.mickleak.taskmanagementsystem.server.api.v1.Task;
-import org.mickleak.taskmanagementsystem.server.api.v1.Task.StatusEnum;
+import org.mickleak.taskmanagementsystem.server.api.v1.TaskPriority;
+import org.mickleak.taskmanagementsystem.server.api.v1.TaskStatus;
 
 
 public class TestsUtils {
 
 	public static Task createTask( final Integer i, final String title ) {
-		return createTask( i, title, StatusEnum.PENDING, Task.PriorityEnum.MEDIUM );
+		return createTask( i, title, TaskStatus.PENDING, TaskPriority.MEDIUM );
 	}
 
 	public static Task createTask( final Integer id,
 	                               final String title,
-	                               final StatusEnum status,
-	                               final Task.PriorityEnum priority ) {
+	                               final TaskStatus status,
+	                               final TaskPriority priority ) {
 		return new Task()
 			.id( id )
 			.title( title )
